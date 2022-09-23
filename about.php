020352628330dab1072e344d3980b8d8fd004c45
Halbepassrol <114188358+Halbepassrol@users.noreply.github.com>
2022-09-23 11:28:48 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>RPCS3 - About</title>
+
+
+
+
+
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Der Plan</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Lesen Sie und werden Supporter!
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Das Ziel</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Die Umsetzung</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+								
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									
+
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				</div>
+			</div>
+			
+		
+</body>
+</html>
